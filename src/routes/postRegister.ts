862ef7dc3b7f4b3942c7efe6c73bcca571a69adc
Tristan-Boudra <boudratristan@gmail.com>
2023-01-04 12:15:40 +0100

    feat(package): add link github

--- src/routes/postRegister.ts
+++ src/routes/postRegister.ts
@@ -1,19 +1,19 @@
-// import { Application } from "express-ws";
-// import bodyParser from "body-parser";
-// import {createUser} from "../repositories/userRepository";
+import { Application } from "express-ws";
+import bodyParser from "body-parser";
+import {createUser} from "../repositories/userRepository";
 
-// export function postRegister(app: Application) {
-//     app.post('/register', bodyParser.urlencoded async (req, res) => {
-//         const {email, name} = req.body;
-//         if(!email || !name){
-//             res.status(400).send('Bad request')
-//             return
-//         }
-//         const user = await createUser(email, name);
-//         if(!user){
-//             res.status(400).send('Email already exists')
-//             return
-//         }
-//         res.cookie('ssid', user.id, {signed: true, httpOnly: true, sameSite: true})
-//     })
-// }
+export function postRegister(app: Application) {
+    app.post('/register', bodyParser.urlencoded async (req, res) => {
+        const {email, name} = req.body;
+        if(!email || !name){
+            res.status(400).send('Bad request')
+            return
+        }
+        const user = await createUser(email, name);
+        if(!user){
+            res.status(400).send('Email already exists')
+            return
+        }
+        res.cookie('ssid', user.id, {signed: true, httpOnly: true, sameSite: true})
+    })
+}
